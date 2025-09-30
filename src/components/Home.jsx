be7ac38f2edb6bd45eef9b0032b7599b71a6ba63
farazazeem-6 <faraz.tacticminds.com>
2025-09-30 18:41:24 +0500

    Home page finish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/Quiz.css";
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>QUIZ APP USING REACT</h1>
-      <p>Select Difficulty</p>
-      <button onClick={() => navigate("/quiz/easy")}>Easy</button>
-      <button onClick={() => navigate("/quiz/medium")}>Medium</button>
-      <button onClick={() => navigate("/quiz/hard")}>Hard</button>
+    <div className="container">
+      <img className="bk-img" src="./src/assets/bk.png" alt="" />
+      <img className="logo-img" src="./src/assets/logo.svg" alt="" />
+      <div className="home-data">
+        <h1 className="home-heading">Welcome to Quiz App</h1>
+        <p className="category">Select Category</p>
+        <div className="btn">
+          <button
+            className="button easy"
+            onClick={() => navigate("/quiz/easy")}
+          >
+            Easy
+          </button>
+          <button
+            className="button medium"
+            onClick={() => navigate("/quiz/medium")}
+          >
+            Medium
+          </button>
+          <button
+            className="button hard"
+            onClick={() => navigate("/quiz/hard")}
+          >
+            Hard
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
